88f7b6f468423602c144a3ffc2f21b716eb87ac0
anhduc-developer <duckuro.ptit@gmail.com>
2026-02-19 14:27:12 +0700

    sure

---
+++ Back-end/src/test/java/vn/Backend/TTCS_PTIT/TtcsPtitApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.Backend.TTCS_PTIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtcsPtitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
